b9b46aaed054a4af0f9941c9ddfea4accc7411cc
Simon Huang <simon@simonhuang.ca>
2020-07-25 02:18:57 -0400

    Update port and scripts

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', userRouter);
 app.use('/', bookRouter);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
