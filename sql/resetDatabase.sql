045ed259c440b4837bb5bcfd7c193a09d15d45df
seanheinrichs <seanheinrichs@gmail.com>
2020-07-30 12:55:36 -0400

    finalize schema

--- sql/resetDatabase.sql
+++ sql/resetDatabase.sql
@@ -1,16 +1,18 @@
 -- RUNNING THIS SCRIPT RESETS THE DATABASE
 
 -- Before running this script ensure to replace the statement below with your schema name
-USE <SCEHMA TO BE USED>;
-
+-- USE <SCEHMA TO BE USED>;
+USE assignment1;
 -- --------------------------------------------------
 -- Drop all previous tables (ORDER MATTERS)
 -- --------------------------------------------------
 
-DROP TABLE IF EXISTS `Application`;
+DROP TABLE IF EXISTS `Applicants`;
 DROP TABLE IF EXISTS `Job`;
 DROP TABLE IF EXISTS `Category`;
-DROP TABLE IF EXISTS `Payment`;
+DROP TABLE IF EXISTS `CheckingAccount`;
+DROP TABLE IF EXISTS `CreditCard`;
+DROP TABLE IF EXISTS `PaymentMethod`;
 DROP TABLE IF EXISTS `Employer`;
 DROP TABLE IF EXISTS `Employee`;
 DROP TABLE IF EXISTS `Admin`;
@@ -22,55 +24,50 @@ DROP TABLE IF EXISTS `Subscription`;
 -- --------------------------------------------------
 
 CREATE TABLE `Subscription`(
-  `subscriptionID` VARCHAR(255) NOT NULL,
-  -- ADD ATTRIBUTES: s
-  `limit` INT,
-  `cost` INT,
+  `subscriptionID` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255),
+  `cost` DECIMAL,
   PRIMARY KEY (`subscriptionID`)
 );
 
 CREATE TABLE `User`(
   `userName` VARCHAR(255) NOT NULL,
-  `subscriptionID` VARCHAR(255) NOT NULL,
-  -- ADD ATTRIBUTES: u
-
+  `subscriptionID` INT NOT NULL,
+  `password` VARCHAR(255),
+  `email` VARCHAR(255),
+  `firstName` VARCHAR(255),
+  `lastName` VARCHAR(255),
+  `privilege` ENUM('admin', 'employer', 'employee'),
+  `balance` DECIMAL,
+  `suffering` BOOLEAN,
+  `active` BOOLEAN,
   PRIMARY KEY (`userName`),
   FOREIGN KEY (`subscriptionID`) REFERENCES `Subscription`(`subscriptionID`)
 );
 
-CREATE TABLE `Admin`(
-  `adminID` VARCHAR(255) NOT NULL,
+CREATE TABLE `PaymentMethod`(
+  `paymentID` INT NOT NULL AUTO_INCREMENT,
   `userName` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`adminID`),
-  FOREIGN KEY (`userName`) REFERENCES `User`(`userName`)
-);
-
-CREATE TABLE `Employee`(
-  `employeeID` VARCHAR(255) NOT NULL,
-  `userName` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`employeeID`),
-  FOREIGN KEY (`userName`) REFERENCES `User`(`userName`)
-);
-
-CREATE TABLE `Employer`(
-  `employerID` VARCHAR(255) NOT NULL,
-  `userName` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`employerID`),
-  FOREIGN KEY (`userName`) REFERENCES `User`(`userName`)
-);
-
-CREATE TABLE `Payment`(
-  `paymentID` VARCHAR(255) NOT NULL,
-  `userName` VARCHAR(255) NOT NULL,
-  -- ADD ATTRIBUTES: a (credit card)
-  `creditCardNumber` VARCHAR(255),
-  `expirationDate` VARCHAR(255),
-  `cvv` VARCHAR(255),
-   -- ADD ATTRIBUTES: b (bank account)
-  `accountNumber` VARCHAR(255),
-  `Bank` VARCHAR(255),
+  `privilege` ENUM('cheqing', 'credit'),
+  `lastPayment` DATE,
   PRIMARY KEY (`paymentID`),
   FOREIGN KEY (`userName`) REFERENCES `User`(`userName`)
+);
+
+CREATE TABLE `CreditCard`(
+  `creditCardNumber` VARCHAR(255) NOT NULL,
+  `paymentID` INT NOT NULL,
+  `expirationDate` VARCHAR(255),
+  `cvv` INT,  
+  PRIMARY KEY (`creditCardNumber`),
+  FOREIGN KEY (`paymentID`) REFERENCES `PaymentMethod`(`paymentID`)
+);
+
+CREATE TABLE `CheckingAccount`(
+  `accountNumber` VARCHAR(255) NOT NULL,
+  `paymentID` INT NOT NULL,
+  PRIMARY KEY (`accountNumber`),
+  FOREIGN KEY (`paymentID`) REFERENCES `PaymentMethod`(`paymentID`)
 );
 
 CREATE TABLE `Category`(
@@ -79,25 +76,23 @@ CREATE TABLE `Category`(
 );
 
 CREATE TABLE `Job`(
-  `jobID` VARCHAR(255) NOT NULL,
-  `employerID` VARCHAR(255) NOT NULL,
-  `categoryName` VARCHAR(255),
-  -- ADD ATTRIBUTES: j
+  `jobID` INT NOT NULL AUTO_INCREMENT,
+  `userName` VARCHAR(255) NOT NULL,
+  `categoryName` VARCHAR(255) NOT NULL,
   `title` VARCHAR(255),
   `datePosted` DATE,
   `description` VARCHAR(255), -- MORE THAN 255?
-  `employeeNeeded` INT,
+  `employeesNeeded` INT,
   PRIMARY KEY (`jobID`),
-  FOREIGN KEY (`employerID`) REFERENCES `Employer`(`employerID`),
+  FOREIGN KEY (`userName`) REFERENCES `User`(`userName`),
   FOREIGN KEY (`categoryName`) REFERENCES `Category`(`categoryName`)
 );
 
-CREATE TABLE `Application`(
-  `employeeID` VARCHAR(255) NOT NULL,
-  `jobID` VARCHAR(255) NOT NULL,
-  -- ADD ATTRIBUTES: x
+CREATE TABLE `Applicants`(
+  `userName` VARCHAR(255) NOT NULL,
+  `jobID` INT NOT NULL,
   `status` ENUM('pending', 'rejected', 'hired', 'withdrawn'),
-  PRIMARY KEY (`employeeID`, `jobID`),
-  FOREIGN KEY (`employeeID`) REFERENCES `Employee`(`employeeID`),
+  PRIMARY KEY (`userName`, `jobID`),
+  FOREIGN KEY (`userName`) REFERENCES `User`(`userName`),
   FOREIGN KEY (`jobID`) REFERENCES `Job`(`jobID`)
 );
