de63414f98238d4d25d43c41340b108a0dd2025c
Simon Huang <simon@simonhuang.ca>
2020-08-07 21:52:12 -0400

    Add reset password endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -7,5 +7,6 @@ const authController = require("../controllers/authController");
 router.post("/login", authController.login);
 router.post("/register", authController.register);
 router.post("/logout", authController.logout);
+router.post("/resetPassword", authController.resetPassword);
 
 module.exports = router;
